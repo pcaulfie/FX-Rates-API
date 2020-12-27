ecff205715fe14ee1e1d542b11661acb08028461
Paul Caulfield <SESA87346@se.com>
2020-12-27 16:03:30 +0000

    removed connection pool

--- deploytoazure/OrdersDao.py
+++ deploytoazure/OrdersDao.py
@@ -1,32 +1,26 @@
 import mysql.connector
+from mysql.connector import cursor
 import dbconfig as cfg
 
 class OrdersDao:
-    
-    def initConnectToDB(self):
-        db = mysql.connector.connect(
+    db = ""
+    def connectToDB(self):
+        self.db = mysql.connector.connect(
             host=       cfg.mysql['host'],
             user=       cfg.mysql['username'],
             password=   cfg.mysql['password'],
-            database=   cfg.mysql['database'],
-            pool_name='my_connection_pool',
-            pool_size=10
+            database=   cfg.mysql['database']
         )
-        return db
-
-    def getConnection(self):
-        db = mysql.connector.connect(
-            pool_name='my_connection_pool'
-        )
-        return db
-
     def __init__(self): 
-        db=self.initConnectToDB()
-        db.close()
+        self.connectToDB()   
+    
+    def getCursor(self):
+        if not self.db.is_connected():
+            self.connectToDB()
+        return self.db.cursor()
 
     def create(self, customer):
-        db = self.getConnection()
-        cursor = db.cursor()
+        cursor = self.getCursor()
         sql = "insert into customer (CUSTOMER_NAME, ZONE) values (%s,%s)"
         values = [
             customer['CUSTOMER_NAME'],
@@ -35,12 +29,11 @@ class OrdersDao:
         cursor.execute(sql, values)
         self.db.commit()
         lastRowId=cursor.lastrowid
-        db.close()
+        cursor.close()
         return lastRowId
 
     def create(self, orders):
-        db = self.getConnection()
-        cursor = db.cursor()
+        cursor = self.getCursor()
         sql = "insert into orders (ORDER_NUMBER,CUSTOMER_NUMBER,PART,CURR_CODE,OPEN_QTY,UNIT_PRICE_USD,USD_EUR_FXRATE) values (%s,%s,%s,%s,%s,%s,%s)"
         values = [
             orders['ORDER_NUMBER'],
@@ -54,12 +47,11 @@ class OrdersDao:
         cursor.execute(sql, values)
         self.db.commit()
         lastRowId=cursor.lastrowid
-        db.close()
+        cursor.close()
         return lastRowId
 
     def getAll(self):
-        db = self.getConnection()
-        cursor = db.cursor()
+        cursor = self.getCursor()
         sql = 'select * from orders'
         cursor.execute(sql)
         results = cursor.fetchall()
@@ -68,7 +60,7 @@ class OrdersDao:
         for result in results:
             resultAsDict = self.convertToDict(result)
             returnArray.append(resultAsDict)
-        db.close()
+        cursor.close()
         return returnArray
 
     def convertToDict(self, result):
@@ -82,19 +74,17 @@ class OrdersDao:
         return order
 
     def findById(self, ID):
-        db = self.getConnection()
-        cursor = db.cursor()
+        cursor = self.getCursor()
         sql = 'select * from orders where ID = %s'
         values = [ID]
         cursor.execute(sql, values)
         result = cursor.fetchone()
         order=self.convertToDictionary(result)
-        db.close()
+        cursor.close()
         return order
 
     def updateById(self, orders):
-        db = self.getConnection()
-        cursor = db.cursor()
+        cursor = self.getCursor()
         sql = "update orders set ORDER_NUMBER = %s, CUSTOMER_NUMBER = %s, PART = %s, CURR_CODE = %s, OPEN_QTY = %s, UNIT_PRICE_USD = %s,USD_EUR_FXRATE = %s where ID = %s"
         values = [
             orders['ORDER_NUMBER'],
@@ -108,12 +98,10 @@ class OrdersDao:
         ]
         cursor.execute(sql, values)
         self.db.commit()
-        db.close()
-        return orders
+        cursor.close()
 
     def update(self, orders):
-       db = self.getConnection()
-       cursor = db.cursor()
+       cursor = self.getCursor()
        sql = "update orders set USD_EUR_FXRATE = %s"
        values = [
            orders['USD_EUR_FXRATE']
@@ -121,18 +109,15 @@ class OrdersDao:
        ]
        cursor.execute(sql, values)
        self.db.commit()
-       db.close()
-       return orders
+       cursor.close()
 
     def delete(self, ID):
-       db = self.getConnection()
-       cursor = db.cursor()
+       cursor = self.getCursor()
        sql = 'delete from orders where ID = %s'
        values = [ID]
        cursor.execute(sql, values)
        self.db.commit()
-       db.close()
-       return {}
+       cursor.close()
 
 ordersDao = OrdersDao()    
 
